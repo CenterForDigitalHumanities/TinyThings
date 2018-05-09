6d6a2d222a51a1b3957be17c398196336cccb6f2
Bryan Haberberger <bhaberbe@slu.edu>
2018-05-09 14:05:21 -0500

    A tiny backend

---
+++ Source Packages/crud/Constant.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+/**
+ *
+ * @author bhaberbe
+ */
+public class Constant {
+    public static String API_ADDR = "http://devstore.rerum.io/v1/api";
+}
