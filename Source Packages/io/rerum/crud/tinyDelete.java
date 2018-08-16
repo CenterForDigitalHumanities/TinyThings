b281f4e6a2cfb4a66f5bc8e40d0d0e6f96769d68
Bryan Haberberger <bhaberbe@slu.edu>
2018-08-16 12:53:43 -0500

    Changes to forward RERUM responses through the tiny proxy API.

--- Source Packages/io/rerum/crud/tinyDelete.java
+++ Source Packages/io/rerum/crud/tinyDelete.java
@@ -37,62 +37,76 @@ public class tinyDelete extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, Exception {
-        
+        response.setHeader("Content-Type", "application/json; charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
         TinyTokenManager manager = new TinyTokenManager();
         manager.init();
         BufferedReader bodyReader = request.getReader();
         StringBuilder bodyString = new StringBuilder();
         String line;
         String requestString;
+        StringBuilder sb = new StringBuilder();
+        int codeOverwrite = 500;
+        boolean moveOn = true;
         while ((line = bodyReader.readLine()) != null)
         {
           bodyString.append(line);
         }
         requestString = bodyString.toString();
+        
+        //RERUM will catch this
+        /*
         if(!requestString.contains(Constant.RERUM_ID_PATTERN)){
             //IT IS NOT a rerum object, we can't delete this
-            request.sendError(HttpServletResponse.SC_BAD_REQUEST,"Your provided id must be a RERUM URL. Pattern \""+Constant.RERUM_ID_PATTERN+"\" was not found.");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Your provided id must be a RERUM URL. Pattern \""+Constant.RERUM_ID_PATTERN+"\" was not found.");
+            moveOn = false;
             // If we throw instead, the return isn't needed.
-            return;
         }
+        */
         //If it was JSON
-            String pubTok = manager.getAccessToken();
-            boolean expired = manager.checkTokenExpiry();
-            if(expired){
-                System.out.println("Tiny thing detected an expired token, auto getting and setting a new one...");
-                pubTok = manager.generateNewAccessToken();
-            }
-            //Point to rerum server v1
-            URL postUrl = new URL(Constant.RERUM_API_ADDR + "/delete.action");
-            HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestMethod("DELETE");
-            connection.setUseCaches(false);
-            connection.setInstanceFollowRedirects(true);
-            connection.setRequestProperty("Authorization", "Bearer "+pubTok);
-            connection.connect();
+        String pubTok = manager.getAccessToken();
+        boolean expired = manager.checkTokenExpiry();
+        if(expired){
+            System.out.println("Tiny thing detected an expired token, auto getting and setting a new one...");
+            pubTok = manager.generateNewAccessToken();
+        }
+        //Point to rerum server v1
+        URL postUrl = new URL(Constant.RERUM_API_ADDR + "/delete.action");
+        HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("DELETE");
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestProperty("Authorization", "Bearer "+pubTok);
+        connection.connect();
+        try{
             DataOutputStream out = new DataOutputStream(connection.getOutputStream());
             //Pass in the user provided JSON for the body of the rerumserver v1 request
             out.writeBytes(requestString);
             out.flush();
             out.close(); 
             //Execute rerum server v1 request
+            codeOverwrite = connection.getResponseCode();
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
-            StringBuilder sb = new StringBuilder();
             while ((line = reader.readLine()) != null){
                 //Gather rerum server v1 response
                 sb.append(line);
             }
             reader.close();
-            int code = connection.getResponseCode();
-            connection.disconnect();
-            //Hand back rerumserver response as this API's response.
-            // https://github.com/CenterForDigitalHumanities/TinyThings/issues/4
-            // Should we check `code` for 204? Throw something?
-            response.setStatus(code);
-            response.setContentType("application/json");
-            response.getWriter().print(sb.toString());
+        }
+        catch(IOException ex){
+            //Need to get the response RERUM sent back.
+            BufferedReader error = new BufferedReader(new InputStreamReader(connection.getErrorStream(),"utf-8"));
+            String errorLine = "";
+            while ((errorLine = error.readLine()) != null){  
+                sb.append(errorLine);
+            } 
+            error.close();
+        }
+        connection.disconnect();
+        response.setStatus(codeOverwrite);
+        response.getWriter().print(sb.toString());
     }
 
     /**
