9f2cf14b8e9f776839a1e220aaf8aae18cfd6224
Bryan Haberberger <bhaberbe@slu.edu>
2018-11-02 09:13:07 -0500

    Don't need .action

--- Source Packages/io/rerum/crud/Constant.java
+++ Source Packages/io/rerum/crud/Constant.java
@@ -13,8 +13,8 @@ public class Constant {
     public static String RERUM_REGISTRATION_URL = "http://devstore.rerum.io/v1/";
     public static String RERUM_API_ADDR = "http://devstore.rerum.io/v1/api";
     //public static String RERUM_ID_PATTERN = "//devstore.rerum.io/v1/id";
-    public static String RERUM_ACCESS_TOKEN_URL = "http://devstore.rerum.io/v1/api/accessToken.action";
-    public static String RERUM_REFRESH_TOKEN_URL = "http://devstore.rerum.io/v1/api/refreshToken.action";
+    public static String RERUM_ACCESS_TOKEN_URL = "http://devstore.rerum.io/v1/api/accessToken";
+    public static String RERUM_REFRESH_TOKEN_URL = "http://devstore.rerum.io/v1/api/refreshToken";
     
     //https://stackoverflow.com/questions/2395737/java-relative-path-of-a-file-in-a-java-web-application
     public static String PROPERTIES_FILE_NAME = "tiny.properties";
