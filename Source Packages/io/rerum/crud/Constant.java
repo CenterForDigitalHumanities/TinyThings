aa7644eb1bbf427eacdaaafed9adb6ae18669b4e
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-02-17 11:16:48 -0600

    Test connection between prd02 and img01

--- Source Packages/io/rerum/crud/Constant.java
+++ Source Packages/io/rerum/crud/Constant.java
@@ -11,11 +11,11 @@ package io.rerum.crud;
  */
 public class Constant {
     //AWS   http://18.218.227.205:8080/rerum_server
-    public static String RERUM_REGISTRATION_URL = "http://devstore.rerum.io/v1/";
-    public static String RERUM_API_ADDR = "http://devstore.rerum.io/v1/api";
-    //public static String RERUM_ID_PATTERN = "//devstore.rerum.io/v1/id";
-    public static String RERUM_ACCESS_TOKEN_URL = "http://devstore.rerum.io/v1/api/accessToken.action";
-    public static String RERUM_REFRESH_TOKEN_URL = "http://devstore.rerum.io/v1/api/refreshToken.action";
+    public static String RERUM_REGISTRATION_URL = "http://test-store.rerum.io/prd02-img01/";
+    public static String RERUM_API_ADDR = "http://test-store.rerum.io/prd02-img01/api";
+    //public static String RERUM_ID_PATTERN = "//test-store.rerum.io/v1/id";
+    public static String RERUM_ACCESS_TOKEN_URL = "http://test-store.rerum.io/prd02-img01/api/accessToken.action";
+    public static String RERUM_REFRESH_TOKEN_URL = "http://test-store.rerum.io/prd02-img01/api/refreshToken.action";
     
     //https://stackoverflow.com/questions/2395737/java-relative-path-of-a-file-in-a-java-web-application
     public static String PROPERTIES_FILE_NAME = "tiny.properties";
